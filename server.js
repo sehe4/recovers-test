dd1bbfd6af0e9a5dc44ff113682ef8c097a81bcd
sehe4 <shernandezv@uc.cl>
2023-06-14 00:06:46 -0400

    all

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const { Pool } = require('pg');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
 
 // PostgreSQL connection configuration
 const pool = new Pool({
