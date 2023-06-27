d0190a9b9403334f0b97f89f8bc14b02c03db0ae
sehe4 <shernandezv@uc.cl>
2023-06-26 23:31:48 -0400

    update web service

--- server.js
+++ server.js
@@ -1,34 +1,37 @@
 const express = require('express');
-const { Pool } = require('pg');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 const port = 5000;
 
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 app.use(cors());
 app.use(bodyParser.json());
 
-// PostgreSQL connection configuration
-const pool = new Pool({
-  user: 'recovers',
-  host: 'dpg-ci4evdlgkuvm71dnn7l0-a.oregon-postgres.render.com',
-  database: 'recovers_db_wtgk',
-  password: 'kndennjN9I9WDeSriMeiw88nk4FdBfwV',
-  port: 5432
-});
+let latestData = {}; // Variable to store the latest data
 
-// Removed duplicate middleware
-app.use(express.json());
+// Endpoint to serve the HTML page
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
 // API endpoint to handle the POST request
 app.post('/insert', (req, res) => {
-  console.log("Inserting data");
-  console.log(req.body);
+  console.log("Updating data");
+
+  // Process the incoming JSON data
+  latestData = req.body;
+  // Emit the latest data to connected clients via WebSocket
+  io.emit('dataUpdate', latestData);
+
   res.sendStatus(200); // Send a response status of 200 OK
 });
 
 // Start the server
-app.listen(port, () => {
+http.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
