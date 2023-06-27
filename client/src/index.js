16413a5d9bbdf422e099f9fbc61e7a10d95672b4
sehe4 <shernandezv@uc.cl>
2023-06-27 13:05:05 -0400

    update final

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+document.title = "Data Display Recovers"; // Update the document title
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
